f005d51e8bc7c306a44af8c21e4f417ad9d1fcb7
Datta Angad Shinge <146843656+Dattashinge@users.noreply.github.com>
2026-06-16 20:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    if (year % 400 === 0) {
+        return 366;
+    } else if (year % 100 === 0) {
+        return 365;
+    } else if (year % 4 === 0) {
+        return 366;
+    } else {
+        return 365;
+    }
+}
+console.log(daysOfAYear(2022)); // 365
+console.log(daysOfAYear(2024)); // 366
+console.log(daysOfAYear(1900)); // 365
+console.log(daysOfAYear(2000)); // 366
